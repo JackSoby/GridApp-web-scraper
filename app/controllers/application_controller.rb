a5736fd8b2bfd032f99949ee1724e00752a6f797
Luigi Lake <lfdlake@gmail.com>
2017-12-10 18:26:01 -0500

    find body text

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
     require 'open-uri'
     doc = Nokogiri::HTML(open("https://www.mangoo.org/product-catalogue/productdetail/market/show/product/s20-solar-lamp/?tx_marketplace_articlesearch%5Bcontroller%5D=Product&cHash=dec2b667b8c66f18d9b28aafd0a09c28"))
     product_name = doc.css('.col-xs-12 h1')
-    render html: product_name
+    product_description = doc.css('p.bodytext')[0]
+    render html: product_description[0]
   end
 end
