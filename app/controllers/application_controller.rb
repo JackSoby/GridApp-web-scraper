aacbda0af9f812b8e788937cd4502499a6770eec
Luigi Lake <lfdlake@gmail.com>
2017-12-10 18:21:48 -0500

    find product name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   def scrape_mangoo
     require 'open-uri'
     doc = Nokogiri::HTML(open("https://www.mangoo.org/product-catalogue/productdetail/market/show/product/s20-solar-lamp/?tx_marketplace_articlesearch%5Bcontroller%5D=Product&cHash=dec2b667b8c66f18d9b28aafd0a09c28"))
-    render html: doc
+    product_name = doc.css('.col-xs-12 h1')
+    render html: product_name
   end
 end
