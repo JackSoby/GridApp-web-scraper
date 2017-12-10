be4100a9108192816b24f28c8a876bb98f7103ac
Luigi Lake <lfdlake@gmail.com>
2017-12-10 18:50:43 -0500

    find product features

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,17 @@ class ApplicationController < ActionController::Base
   def scrape_mangoo
     require 'open-uri'
     doc = Nokogiri::HTML(open("https://www.mangoo.org/product-catalogue/productdetail/market/show/product/s20-solar-lamp/?tx_marketplace_articlesearch%5Bcontroller%5D=Product&cHash=dec2b667b8c66f18d9b28aafd0a09c28"))
-    product_name = doc.css('.col-xs-12 h1')
-    product_description = doc.css('p.bodytext')[0]
-    render html: product_description[0]
+    product_name = doc.css('.col-xs-12 h1').text
+    product_description = doc.css('p.bodytext')[0].text
+
+    # iterate through indexes 4 - 9 in css array result
+    product_features = []
+    5.times { |i|
+      product_features << doc.css('div.row div.col-xs-12 p')[i + 4].text
+    }
+
+    manufacturers_site = doc.css('div.row div.col-xs-12 p a')
+
+    render html: manufacturers_site
   end
 end
