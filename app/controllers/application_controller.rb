43133f111c6c0ace76fcb7ccfaed54e7fe3a631f
Luigi Lake <lfdlake@gmail.com>
2017-12-10 18:15:07 -0500

    parse sample solar lamp site

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def scrape_mangoo
-    render html: 'scrape mangoo data here'
+    require 'open-uri'
+    doc = Nokogiri::HTML(open("https://www.mangoo.org/product-catalogue/productdetail/market/show/product/s20-solar-lamp/?tx_marketplace_articlesearch%5Bcontroller%5D=Product&cHash=dec2b667b8c66f18d9b28aafd0a09c28"))
+    render html: doc
   end
 end
