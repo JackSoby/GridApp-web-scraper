4a8ea4d0c1008299b2e16e1512ba5fac0b01dcbf
Luigi Lake <lfdlake@gmail.com>
2017-12-11 19:15:05 -0500

    fix description

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,21 @@ class ApplicationController < ActionController::Base
 
   def scrape_mangoo
     require 'open-uri'
-    doc = Nokogiri::HTML(open("https://www.mangoo.org/product-catalogue/productdetail/market/show/product/s2-solar-lamp/?tx_marketplace_articlesearch%5Bcontroller%5D=Product&cHash=edb224d00917c9e9ea2fb69977ae606c"))
-    
-    # iterate through indexes 4 - 9 in css array result
-    product_features = []
-    5.times { |i|
-      product_features << doc.css('div.row div.col-xs-12 p')[i + 4].text.gsub("\r", "")
-    }
+    doc = Nokogiri::HTML(open("https://www.mangoo.org/product-catalogue/productdetail/market/show/product/pico-solar-home-system-3000/?tx_marketplace_articlesearch%5Bcontroller%5D=Product&cHash=28d7ac98bfcd74cd35751bee7121bf29"))
 
+    description = doc.css('p')
+
+    # iterate through indexes 4 - 9 in css array result to pull PRODUCT FEATURES
+    product_features = []
+    doc.css('div.row div.col-xs-12 p').each_with_index do |info, index|
+      if info.text.include?("Model")
+        6.times { |i|
+          product_features << doc.css('div.row div.col-xs-12 p')[index + i].text.gsub("\r", "")
+        }
+      end
+    end
+
+    # pull all DISTRIBUTOR INFORMATION
     distributors = []
     doc.css('div.col-xs-12 td').each_with_index do |info, index|
       if index % 4 == 0
@@ -23,19 +30,28 @@ class ApplicationController < ActionController::Base
       end
     end
 
+    # MANUFACTURER'S WEBSITE
+    manufacturer = doc.css('div.col-xs-12 p a').find do |p|
+      p.text.include?("Manufacturer")
+    end
+
+    # LIGHTING GLOBAL WEBSITE
+    lighting_global = doc.css('a').find do |p|
+      p["href"].include?("lightingglobal")
+    end
+
     data = {
       name: doc.css('.col-xs-12 h1').text,
       description: doc.css('p.bodytext')[0].text,
       features: product_features,
       availability: doc.css('div.col-xs-12 p')[doc.css('div.col-xs-12 p').length - 1].text.gsub("\n", "").gsub("   ", ""),
       links: {
-        manufacturer: doc.css('div.col-xs-12 p a')[1]["href"],
-        lighting_global: doc.css('div.col-xs-12 p a')[2]["href"],
-        awango: doc.css('div.col-xs-12 p a')[3]["href"]
+        manufacturer: manufacturer["href"],
+        lighting_global: lighting_global["href"]
       },
       distributors: distributors
     }
 
-    render json: data
+    render html: description
   end
 end
