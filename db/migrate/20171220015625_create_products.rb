1ae66b3155ae3504e00df3a93c4c98b8ac2924c8
Luigi Lake <lfdlake@gmail.com>
2017-12-20 18:34:36 -0500

    add revisions to product table with notes

--- db/migrate/20171220015625_create_products.rb
+++ db/migrate/20171220015625_create_products.rb
@@ -3,21 +3,30 @@ class CreateProducts < ActiveRecord::Migration[5.1]
     create_table :products do |t|
       t.string :name, null: false
       t.text :description
-      t.string :model_num_m
-      t.integer :panel_size_m
-      t.integer :batt_size_ah_m
-      t.integer :batt_size_v_m
-      t.string :batt_type_m
-      t.integer :lumens_m
-      t.boolean :mobile_charge_lg
-      t.integer :lights_lg
-      t.string :solar_lg
-      t.string :batt_type_lg
-      t.string :warranty
-      t.date :expiration_lg
-      t.string :type
-      t.float :batt_time
-      t.string :light_distribution
+      t.string :model_num_m # S20
+      t.integer :panel_size_m # 0.4
+      t.integer :batt_size_ah_m # 0.4
+      t.integer :batt_size_v_m # 3.2 -- PDF: Battery nominal voltage
+      t.string :batt_type_m # lithium iron phosphate X
+      t.integer :lumens_m # 29
+      t.boolean :mobile_charge_lg # false
+      t.integer :num_lights_lg # 1
+      t.string :solar_lg #inbuilt
+      t.string :batt_type_lg #lithium iron phosphate X
+      t.string :warranty #24 months
+      t.date :expiration_lg # june 30, 2018
+      t.string :lamp_type # LED
+      t.float :batt_time # (lumen-hours/solar-day) 130 -- total_light_service
+      t.string :light_distribution # wide
+
+      t.boolean :port_5v
+      t.boolean :port_12v
+      t.boolean :pay_go
+      t.boolean :batt_replacable # false
+
+      t.string :manufacturer # d.light design
+      t.string :manufacturer_site # www.dlight.com
+      t.string :panel_type # monocrystalline silicon
     end
   end
 end
