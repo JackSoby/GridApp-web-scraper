8309c427ad0329b3c4088eefeb31571db13d3ee9
Luigi Lake <lfdlake@gmail.com>
2017-12-20 10:04:47 -0500

    add columns to locations table

--- db/migrate/20171220015716_create_locations.rb
+++ db/migrate/20171220015716_create_locations.rb
@@ -1,6 +1,11 @@
 class CreateLocations < ActiveRecord::Migration[5.1]
   def change
     create_table :locations do |t|
+      t.string :continent
+      t.string :country, null: false
+      t.string :city
     end
+
+    add_index :locations, [:continent, :country, :city], unique: true
   end
 end
