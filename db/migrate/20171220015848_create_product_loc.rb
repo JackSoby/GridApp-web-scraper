83262dbed1a0a654d2b4239299d13f4242a7d85d
Luigi Lake <lfdlake@gmail.com>
2017-12-20 10:07:50 -0500

    add foreign key columsn to product_locations table

--- db/migrate/20171220015848_create_product_loc.rb
+++ db/migrate/20171220015848_create_product_loc.rb
@@ -1,6 +1,10 @@
 class CreateProductLoc < ActiveRecord::Migration[5.1]
   def change
     create_table :product_locs do |t|
+      t.belongs_to :product
+      t.belongs_to :location
     end
+
+    add_index :product_locs, [:product, :location], unique: true
   end
 end
