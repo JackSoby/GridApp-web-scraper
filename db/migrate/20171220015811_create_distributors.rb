6cbce256ff4b041f7d923370b32034b43ca62f86
Luigi Lake <lfdlake@gmail.com>
2017-12-20 10:06:03 -0500

    add columns to distributor table

--- db/migrate/20171220015811_create_distributors.rb
+++ db/migrate/20171220015811_create_distributors.rb
@@ -1,6 +1,9 @@
 class CreateDistributors < ActiveRecord::Migration[5.1]
   def change
     create_table :distributors do |t|
+      t.string :name, null: false
     end
+
+    add_index :distributors, :name, unique: true
   end
 end
