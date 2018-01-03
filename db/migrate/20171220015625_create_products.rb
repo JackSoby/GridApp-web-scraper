38f4e5325f305fcd754b2f2fa010e030ac916d50
Luigi Lake <lfdlake@gmail.com>
2018-01-02 20:17:26 -0500

    refactor product database to info, battery, light, panel, misc

--- db/migrate/20171220015625_create_products.rb
+++ db/migrate/20171220015625_create_products.rb
@@ -2,39 +2,32 @@ class CreateProducts < ActiveRecord::Migration[5.1]
   def change
     create_table :products do |t|
       t.string :name, null: false
+      t.string :model_num_m # S20
       t.string :product_id, null: false # product id from lighting global url
       t.text :description
-      t.string :model_num_m # S20
+      t.string :image
       t.float :panel_size_m # 0.4
       t.float :batt_size_ah_m # 0.4
       t.float :batt_size_v_m # 3.2 -- PDF: Battery nominal voltage
       t.float :batt_size_wh # batt_size_v_m * batt_size_ah_m
       t.string :batt_capacity # reference battery capacity in pdf for later use
-      t.integer :lumens_m # 29
-      t.boolean :mobile_charge # false
-      t.integer :num_lights # 1
-      t.string :solar #inbuilt
       t.string :batt_type #lithium iron phosphate
-      t.string :warranty #24 months
-      t.date :expiration # june 30, 2018
-      t.string :lamp_type # LED
-      t.float :total_light_service # (lumen-hours/solar-day) 130 -- total_light_service
-      t.string :light_distribution # wide
+      t.boolean :batt_replacable # false
       t.boolean :port_5v # allow null
       t.boolean :port_12v # allow null
-      t.boolean :pay_go # look for pay as go in page
-      t.boolean :batt_replacable # false
+      t.string :solar #inbuilt
       t.string :panel_type # monocrystalline silicon
-      t.string :image
+      t.integer :lumens_m # 29
+      t.integer :num_lights # 1
+      t.string :lamp_type # LED
+      t.float :total_light_service # (lumen-hours/solar-day) 130 -- total_light_service
+      t.float :illumination_area # total area of illumination m2
+      t.string :light_distribution # wide
+      t.boolean :mobile_charge # false
+      t.string :warranty #24 months
+      t.date :expiration # june 30, 2018
+      t.boolean :pay_go # may or may not be available on the page
       t.belongs_to :manufacturer
-
-      # total area of illumination
-
-      # grab a picture from the lighting global website. try to house those photos in our own database.
-
-      # GET RID OF ALL THE LG
-      # m - mangoo
-      # man - manual
     end
   end
 end
