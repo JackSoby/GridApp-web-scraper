8807ba5bbbf96de118c33f02494db1a31c601bf2
Luigi Lake <lfdlake@gmail.com>
2018-01-03 15:09:27 -0500

    fix scope of uniqueness, test for all relations

--- app/models/distributor.rb
+++ app/models/distributor.rb
@@ -1,10 +1,5 @@
 class Distributor < ApplicationRecord
   validates :name, presence: true, uniqueness: true
-  validates :website, allow_nil: true
-  validates :address, allow_nil: true
-  validates :email, allow_nil: true
-  validates :phone, allow_nil: true
-
 
   has_many :distributor_locs
   has_many :locations, through: :distributor_locs
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,7 +1,6 @@
 class Location < ApplicationRecord
-  validates :continent, allow_nil: true
-  validates :city, allow_nil: true
-  validates :country, presence: true, uniqueness: { scope: :continent, :city}
+  validates :country, presence: true, uniqueness: { scope: [:continent, :city] }
+
 
   has_many :distributor_locs
   has_many :distributors, through: :distributor_locs
