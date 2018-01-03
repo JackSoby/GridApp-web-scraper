7299c1d420340c82eab3a578ddc21e90af6394db
Luigi Lake <lfdlake@gmail.com>
2018-01-02 19:34:34 -0500

    refactor distributor and manufacturer tables to contain contact information

--- db/migrate/20171220015811_create_distributors.rb
+++ db/migrate/20171220015811_create_distributors.rb
@@ -2,10 +2,10 @@ class CreateDistributors < ActiveRecord::Migration[5.1]
   def change
     create_table :distributors do |t|
       t.string :name, null: false
-      # add contact information allow null for all and below
-      # phone 1, 2, 3
-      # address
-      # distributor
+      t.string :website
+      t.string :address
+      t.string :email
+      t.string :phone
     end
 
     add_index :distributors, :name, unique: true
--- db/migrate/20180103001836_create_manufacturers.rb
+++ db/migrate/20180103001836_create_manufacturers.rb
@@ -3,7 +3,11 @@ class CreateManufacturers < ActiveRecord::Migration[5.1]
     create_table :manufacturers do |t|
       t.string :name, null: false
       t.string :website
-      
+      t.string :address
+      t.string :email
+      t.string :phone
     end
+
+    add_index :manufacturers, :name, unique: true
   end
 end
